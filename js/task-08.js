becabe197f30565ad39eac767665897e6332d79d
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2021-07-19 22:10:53 +0300

    done task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,30 +1,37 @@
-// Напиши скрипт создания и очистки коллекции элементов.
-// Пользователь вводит количество элементов в input и нажимает кнопку Создать,
-// после чего рендерится коллекция.При нажатии на кнопку Очистить,
-// коллекция элементов очищается.
-
-// Создай функцию createBoxes(amount),
-// которая принимает 1 параметр amount - число.
-// Функция создает столько div, сколько указано в amount
-// и добавляет их в div#boxes.
-
-// Каждый созданный div:
-
-// Имеет случайный rgb цвет фона
-// Размеры самого первого div - 30px на 30px
-// Каждый следующий div после первого,
-// должен быть шире и выше предыдущего на 10px
-// Создай функцию destroyBoxes(), которая очищает div#boxes.
 
 const controlsBtns = document.querySelectorAll('#controls button')
-const input = document.querySelectorAll('#number')
 const renderBoxesBtn= (controlsBtns[0])
 const destroyBoxesBtn = (controlsBtns[1])
-console.log(input)
+const boxesContainer = document.querySelector('#boxes')
+const amountInput = document.querySelector('#controls input')
 
-// input.addEventListener('input', createBoxes)
+renderBoxesBtn.addEventListener("click", getAmount);
+destroyBoxesBtn.addEventListener("click", destroyBoxes);
 
+function getAmount() {
+  const amount = amountInput.value;
+  createBoxes(amount);
+}
 
-// function createBoxes() {
+function createBoxes(amount) {
+  const basicSize = 30;
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i+=1) {
+   const size = basicSize + i * 10;
+      const div = document.createElement('div');
+      div.style = `width: ${size}px; height: ${size}px`;
+      div.style.backgroundColor = `rgba( ${random()} , ${random()} , ${random()} )`
     
-// }
+    fragment.appendChild(div);
+  }
+  boxesContainer.appendChild(fragment);
+}
+
+function destroyBoxes() {
+    boxesContainer.innerHTML = '';
+    amountInput.value = '';
+}
+
+function random() {
+  return Math.floor(Math.random() * 256);
+}
